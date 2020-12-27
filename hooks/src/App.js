157685cd735e4f7f876e326307eb8da5cf4e23c6
qtKite <jackywen4@gmail.com>
2020-12-27 22:38:28 +1100

    useState with object

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 import HookCounter2 from './components/HookCounter2';
+import HookCounter3 from './components/HookCounter3';
 
 function App() {
   return (
     <div className="App App-header">
-      <HookCounter2/>
+      <HookCounter3/>
     </div>
   );
 }
