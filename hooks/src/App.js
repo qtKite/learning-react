371642bd5e0d3b5623714878a594b371289a81be
qtKite <jackywen4@gmail.com>
2020-12-28 18:03:32 +1100

    hook counter 4

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import HookCounter4 from './components/HookCounter4';
 
-import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
 
 function App() {
   return (
     <div className="App App-header">
-      <HookCounter3/>
+      <HookCounter4/>
     </div>
   );
 }
