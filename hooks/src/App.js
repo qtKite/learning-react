ae7a1f5d268aee345a0ff147a9fa119ac37fa440
qtKite <jackywen4@gmail.com>
2021-01-06 19:30:45 +1100

    datafetching 2 completed

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,13 @@
 import React, { createContext, useReducer } from 'react'
 import './App.css';
 import DataFetching1 from './components/DataFetching1';
+import DataFetching2 from './components/DataFetching2';
 
 
 function App() {
   return (
     <div className="App App-header">
-        <DataFetching1/>
+      <DataFetching2 />
     </div>
   );
 }
